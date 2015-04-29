fa4e8fb6d8fc5693b535e4b4cf075932e3f794b2
pesilva23 <pesilva23@outlook.com>
2015-04-29 07:19:47 -0300

    Implementado método para identificar a melhor dimensão do preview da câmera.

--- QuickPhoto/app/src/main/java/br/com/indirim/quickphoto/CameraFragment.java
+++ QuickPhoto/app/src/main/java/br/com/indirim/quickphoto/CameraFragment.java
@@ -233,7 +233,11 @@ public class CameraFragment extends Fragment implements View.OnClickListener, Su
 
         List<Camera.Size> previewSizes = parameters.getSupportedPreviewSizes();
 
-        Camera.Size previewSize = previewSizes.get(0);
+        Camera.Size previewSize = getOptimalPreviewSize(
+                previewSizes,
+                getResources().getDisplayMetrics().widthPixels,
+                getResources().getDisplayMetrics().heightPixels);
+
         parameters.setPreviewSize(previewSize.width, previewSize.height);
 
         List<String> focus = parameters.getSupportedFocusModes();
@@ -250,5 +254,37 @@ public class CameraFragment extends Fragment implements View.OnClickListener, Su
             camera.release();
             camera = null;
         }
+    }
+
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio=(double)h / w;
+
+        if (sizes == null) return null;
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
     }
 }
